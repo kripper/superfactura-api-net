1e8ab9f2edfe64df28cd0947ff57309828e6b4d3
Christopher Pereira <kripper@imatronix.com>
2016-12-10 00:56:19 -0300

    Initial release

---
+++ API/SuperFacturaAPI/Ejemplo/Ejemplo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperFactura;
+
+namespace Ejemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            API api = new API("usuario@cliente.cl", "contraseña");
+
+            string json = @"
+{
+	""Encabezado"" : {
+		""IdDoc"" : {
+			""TipoDTE"" : ""33""
+		},
+		""Emisor"" : {
+			""RUTEmisor"" : ""99581150-2""
+		},
+		""Receptor"" : {
+			""RUTRecep"" : ""1-9"",
+			""RznSocRecep"" : ""Test"",
+			""GiroRecep"" : ""Giro"",
+			""DirRecep"" : ""Dirección"",
+			""CmnaRecep"" : ""Comuna"",
+			""CiudadRecep"" : ""Ciudad""
+		}
+	},
+	""Detalles"" : [
+		{
+			""NmbItem"" : ""Item 1"",
+			""DscItem"" : ""Descripción del item 1"",
+			""QtyItem"" : ""3"",
+			""UnmdItem"" : ""KG"",
+			""PrcItem"" : ""100""
+		},
+		{
+			""NmbItem"" : ""Item 2"",
+			""DscItem"" : ""Descripción del item 2"",
+			""QtyItem"" : ""5"",
+			""UnmdItem"" : ""KG"",
+			""PrcItem"" : ""65""
+		}
+	]
+}
+";
+
+            APIResult res = api.SendDTE(json, "cer");
+            if (res.ok)
+            {
+                Console.WriteLine("Se creó el DTE con folio " + res.folio);
+            }
+        }
+    }
+}
---
+++ API/SuperFacturaAPI/SuperFacturaAPI/API.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperFactura
+{
+    public class API
+    {
+        private string user, password;
+
+        public API(string user, string password)
+        {
+            this.user = user;
+            this.password = password;
+        }
+
+        public APIResult SendDTE(string data, string ambiente)
+        {
+            APIResult apiResult = new APIResult();
+            apiResult.folio = 123; // Se debe obtener de la salida del comando
+
+            // TODO: Gardar data en archivo temporal en file
+            string file = "temp.txt";
+
+            // Ejecutar comando
+            Process p = new Process();
+
+            p.StartInfo.UseShellExecute = false;
+            // p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            string args = EscapeArgument(user) + " " + EscapeArgument(password) + " " + EscapeArgument(ambiente) + " " + EscapeArgument(file);
+            /* TODO: Soportar opciones adicionales
+            if (options)
+            {
+                args += " " + EscapeArgument(options);
+            }
+            */
+            p.StartInfo.FileName = AppDomain.CurrentDomain.BaseDirectory + "..\\api\\superfactura.exe";
+            p.StartInfo.Arguments = args;
+            try
+            {
+                p.Start();
+            } catch(Exception e)
+            {
+                Console.WriteLine("ERROR: No se pudo ejecutar el comando: " + p.StartInfo.FileName);
+            }
+
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            Console.Out.WriteLine("OUTPUT:\n" + output);
+
+            return apiResult;
+        }
+
+        public string EscapeArgument(string arg)
+        {
+            // TODO: Escapar
+            return "\"" + arg + "\"";
+        }
+    }
+
+    public class APIResult
+    {
+        public int folio; // Entrega el folio asignado al DTE.
+        public bool ok; // Indica que el DTE se generó correctamente.
+    }
+}
