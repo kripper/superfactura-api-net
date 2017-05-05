aa2d9488a15aa1cdee4fdfdf3690473fc5e44e72
Christopher Pereira <kripper@imatronix.com>
2017-05-05 00:29:25 -0300

    Implemented JSON protocol
    Removed superfactura.exe dependencies

--- SuperFacturaAPI/API.cs
+++ SuperFacturaAPI/API.cs
@@ -1,114 +1,177 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Compression;
+using System.Web.Script.Serialization;
+using System.Web;
 
 namespace SuperFactura
 {
-    public class API
-    {
-        private string user, password;
-        string jsonOptions = "";
+	public class API
+	{
+		private string user, password;
+		string jsonOptions = "";
+		string savePDF = null;
+		string saveXML = null;
 
-        public API(string user, string password)
-        {
-            this.user = user;
-            this.password = password;
-        }
+		public API(string user, string password)
+		{
+			this.user = user;
+			this.password = password;
+		}
 
-        public void SetSavePDF(string outputFile)
-        {
-            AddJSONOption("savePDF", outputFile);
-        }
+		public void SetSavePDF(string outputFile)
+		{
+			savePDF = outputFile;
+			AddJSONOption("getPDF", "1");
+		}
 
-        private void AddJSONOption(string key, string val)
-        {
-            if (jsonOptions != "") jsonOptions += ",";
-            jsonOptions += EscapeArgument(key) + ":" + EscapeArgument(val);
-        }
+		public void SetSaveXML(string outputFile)
+		{
+			saveXML = outputFile;
+			AddJSONOption("getXML", "1");
+		}
 
-        public APIResult SendDTE(string data, string ambiente)
-        {
-            AddJSONOption("encoding", "UTF-8");
+		private void AddJSONOption(string key, string val)
+		{
+			if (jsonOptions != "") jsonOptions += ",";
+			jsonOptions += EscapeArgument(key) + ":" + EscapeArgument(val);
+		}
 
-            APIResult apiResult = new APIResult();
+		private string SendRequest(string jsonData, string jsonOptions)
+		{
+			using (WebClient client = new WebClient())
+			{
+				byte[] response = client.UploadValues("https://superfactura.cl?a=json", new NameValueCollection()
+				{
+					{ "user", user },
+					{ "pass", password },
+					{ "content", jsonData },
+					{ "options", jsonOptions }
+				});
 
-            string fileName = System.IO.Path.GetTempFileName();
+				string rawOutput = System.Text.Encoding.UTF8.GetString(response);
 
-            System.IO.StreamWriter file = new System.IO.StreamWriter(fileName);
-            file.WriteLine(data);
-            file.Close();
+				return System.Text.Encoding.UTF8.GetString(Decompress(response));
+			}
+		}
 
-            // Ejecutar comando
-            Process p = new Process();
+		static byte[] Decompress(byte[] gzip)
+		{
+			using (GZipStream stream = new GZipStream(new MemoryStream(gzip),
+													  CompressionMode.Decompress))
+			{
+				const int size = 4096;
+				byte[] buffer = new byte[size];
+				using (MemoryStream memory = new MemoryStream())
+				{
+					int count = 0;
+					do
+					{
+						count = stream.Read(buffer, 0, size);
+						if (count > 0)
+						{
+							memory.Write(buffer, 0, count);
+						}
+					}
+					while (count > 0);
+					return memory.ToArray();
+				}
+			}
+		}
 
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.CreateNoWindow = true;
-            p.StartInfo.RedirectStandardOutput = true;
-            string args = EscapeArgument(user) + " " + EscapeArgument(password) + " " + EscapeArgument(ambiente) + " " + EscapeArgument(fileName);
-            if (jsonOptions != "")
-            {
-                args += " " + EscapeArgument("{" + jsonOptions + "}");
-            }
+		public APIResult SendDTE(string jsonData, string ambiente)
+		{
+			AddJSONOption("ambiente", ambiente);
+			AddJSONOption("encoding", "UTF-8");
 
-            p.StartInfo.FileName = AppDomain.CurrentDomain.BaseDirectory + "libs\\superfactura.exe";
-            p.StartInfo.Arguments = args;
+			APIResult apiResult = new APIResult();
 
-            string output;
+			string output = null;
+			dynamic obj = null;
 
-            try
-            {
-                p.Start();
-                output = p.StandardOutput.ReadToEnd();
-                p.WaitForExit();
+			try
+			{
+				output = SendRequest(jsonData, "{" + jsonOptions + "}");
 
-            } catch(Exception)
-            {
-				throw new Exception("ERROR: No se pudo ejecutar el comando: " + p.StartInfo.FileName);
+				var serializer = new JavaScriptSerializer();
+				serializer.RegisterConverters(new[] { new DynamicJsonConverter() });
 
-            } finally
-            {
-                File.Delete(fileName);
-            }
+				obj = serializer.Deserialize(output, typeof(object));
+				if (obj.ack != "ok")
+				{
+					string text = obj.response.title != "" ? obj.response.title + " - " : "" + obj.response.message;
+					throw new Exception("ERROR: " + text);
+				}
 
-            int folio = ParseFolio(output);
-            if(folio != 0)
-            {
-                apiResult.ok = true;
-                apiResult.folio = folio;
-            } else
-            {
-                throw new Exception(output);
-            }
-            return apiResult;
-        }
+			}
+			catch (Exception e)
+			{
+				throw new Exception("API Error: " + e);
 
-        private string EscapeArgument(string arg)
-        {
-            arg = arg.Replace("\\", "\\\\");
-            arg = arg.Replace("\"", "\\\"");
-            return "\"" + arg + "\"";
-        }
+			}
 
-        private int ParseFolio(string output)
-        {
-            string startToken = "\"folio\":\"";
-            string endToken = "\"";
-            int startIndex = output.IndexOf(startToken);
-            if (startIndex == -1) return 0; // ERROR:
-            int valueIndex = startIndex + startToken.Length;
-            var endIndex = output.IndexOf(endToken, valueIndex);
-            string value = output.Substring(valueIndex, endIndex - valueIndex);
-            return int.Parse(value);
-        }
-    }
+			dynamic appRes = obj.response;
 
-    public class APIResult
-    {
-        public int folio; // Entrega el folio asignado al DTE.
-        public bool ok = false; // Indica que el DTE se generó correctamente.
-    }
+			int folio = Convert.ToInt32(appRes.folio);
+			if (appRes.ok == "1")
+			{
+				apiResult.ok = true;
+				apiResult.folio = folio;
+
+				if (savePDF != null)
+				{
+					WriteFile(savePDF + ".pdf", DecodeBase64(appRes.pdf));
+
+					if (appRes.pdfCedible != null)
+					{
+						WriteFile(savePDF + "-cedible.pdf", DecodeBase64(appRes.pdfCedible));
+					}
+				}
+
+				if (saveXML != null)
+				{
+					WriteFile(saveXML + ".xml", appRes.xml);
+				}
+
+			}
+			else
+			{
+				throw new Exception(output);
+			}
+			return apiResult;
+		}
+
+		private byte[] DecodeBase64(string base64)
+		{
+			return System.Convert.FromBase64String(base64);
+		}
+
+		private void WriteFile(string filename, byte[] content)
+		{
+			using (BinaryWriter writer = new BinaryWriter(File.Open(filename, FileMode.Create)))
+			{
+				writer.Write(content);
+			}
+		}
+
+		private string EscapeArgument(string arg)
+		{
+			arg = arg.Replace("\\", "\\\\");
+			arg = arg.Replace("\"", "\\\"");
+			return "\"" + arg + "\"";
+		}
+	}
+
+	public class APIResult
+	{
+		public int folio; // Entrega el folio asignado al DTE.
+		public bool ok = false; // Indica que el DTE se generó correctamente.
+	}
 }
