074cda1d5a3d2d91bb1e2136663005dfbd868adf
Christopher Pereira <kripper@imatronix.com>
2020-11-13 02:34:20 -0300

    Added documentID TextBox to VB.NET Example

--- Ejemplos/Ejemplo VB/Form1.Designer.vb
+++ Ejemplos/Ejemplo VB/Form1.Designer.vb
@@ -25,6 +25,8 @@ Partial Class Form1
 		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
 		Me.TextBox1 = New System.Windows.Forms.TextBox()
 		Me.Button1 = New System.Windows.Forms.Button()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.tbDocumentID = New System.Windows.Forms.TextBox()
 		Me.SuspendLayout()
 		'
 		'TextBox1
@@ -39,18 +41,37 @@ Partial Class Form1
 		'
 		'Button1
 		'
-		Me.Button1.Location = New System.Drawing.Point(574, 351)
+		Me.Button1.Location = New System.Drawing.Point(574, 390)
 		Me.Button1.Name = "Button1"
 		Me.Button1.Size = New System.Drawing.Size(141, 43)
 		Me.Button1.TabIndex = 1
 		Me.Button1.Text = "Generar DTE"
 		Me.Button1.UseVisualStyleBackColor = True
 		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(13, 363)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(112, 20)
+		Me.Label1.TabIndex = 2
+		Me.Label1.Text = "Document ID :"
+		'
+		'tbDocumentID
+		'
+		Me.tbDocumentID.Location = New System.Drawing.Point(131, 360)
+		Me.tbDocumentID.Name = "tbDocumentID"
+		Me.tbDocumentID.Size = New System.Drawing.Size(131, 26)
+		Me.tbDocumentID.TabIndex = 3
+		Me.tbDocumentID.Text = "F123"
+		'
 		'Form1
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(727, 402)
+		Me.ClientSize = New System.Drawing.Size(727, 445)
+		Me.Controls.Add(Me.tbDocumentID)
+		Me.Controls.Add(Me.Label1)
 		Me.Controls.Add(Me.Button1)
 		Me.Controls.Add(Me.TextBox1)
 		Me.Name = "Form1"
@@ -62,4 +83,6 @@ Partial Class Form1
 
 	Friend WithEvents TextBox1 As TextBox
 	Friend WithEvents Button1 As Button
+	Friend WithEvents Label1 As Label
+	Friend WithEvents tbDocumentID As TextBox
 End Class
--- Ejemplos/Ejemplo VB/Form1.vb
+++ Ejemplos/Ejemplo VB/Form1.vb
@@ -1,10 +1,11 @@
 ﻿Public Class Form1
 	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		' Debe usar su cuenta y contraseña de SuperFactura. Ver: https://blog.superfactura.cl/pasos-para-comenzar
 		Dim api As New SuperFactura.API("cuenta@miempresa.cl", "mypassword")
 
 		' Enviar documentID (importante para evitar documentos duplicados en caso de falla de red y reenvío)
 		' Si se envía un ID ya utilizado, se retornará el mismo documento, en vez de crear uno nuevo.
-		Dim documentID As String = "F123"
+		Dim documentID As String = tbDocumentID.Text
 		api.SetOption("documentID", documentID)
 
 		' Solicitar descarga del PDF
@@ -19,6 +20,7 @@
 		Try
 			Dim res As SuperFactura.APIResult
 			res = api.SendDTE(json, "cer")
+			MsgBox("Se generó el folio " + res.folio.ToString())
 		Catch ex As Exception
 			' IMPORTANTE: Este mensaje se debe mostrar al usuario para poder darle soporte.
 			MsgBox(ex.ToString())
